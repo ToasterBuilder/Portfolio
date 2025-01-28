299e6e4a0b153e5b8912ff35ec1ec882bb4fe3c1
Kevin Wu <wukevin1200@gmail.com>
2025-01-27 22:55:01 -0500

    Add project outlines

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-const skills = {
-  'devicon-csharp-plain': 'C#',
-  'devicon-python-plain': 'Python',
-  'devicon-sqldeveloper-plain': 'SQL',
-  'devicon-git-plain': 'Git',
-  'devicon-bash-plain': 'Bash',
-  'devicon-html5-plain': 'HTML',
-  'devicon-css3-plain': 'CSS',
-  'devicon-javascript-plain': 'Javascript',
-  'devicon-express-original': 'Express',
-  'devicon-react-original': 'React',
-  'devicon-mongodb-plain': 'MongoDB',
-  'devicon-dotnetcore-plain': 'Dotnet',
-  'devicon-kotlin-plain': 'Kotlin',
-  'devicon-linux-plain': 'Linux',
-  'devicon-javascript-plain': 'Java',
-  'devicon-docker-plain': 'Docker'
-}
+import { projects, skills } from './project.js';
 
 const skillsKeys = Object.keys(skills);
-
-const skillsSection = document.getElementsByClassName("skills-section")[0];
+const skillsSection = document.getElementById("skills-section");
 
 for(let i = 0; i < skillsKeys.length; ++i){
   // Create icons
   const icon = document.createElement('i');
   icon.className = skillsKeys[i];
-  icon.textContent = skills[skillsKeys[i]]
+  icon.textContent = skills[skillsKeys[i]];
   skillsSection.appendChild(icon);
+}
+
+const projectsSection = document.getElementById("project-section");
+
+for(let i = 0; i < projects.length; ++i){
+  const icon = document.createElement('i');
+  icon.className = 'fas fa-folder';
+  
+  const text = document.createElement('span');
+  text.textContent = projects[i].name;
+
+  icon.appendChild(text);
+  projectsSection.appendChild(icon);
+  // const img = document.createElement('img');
+  // img.src = projects[i].img;
 }
 
 document.querySelectorAll('.selector .button-start, .selector .button').forEach((button) => {
   button.addEventListener('click', () => {
     // Get the section to display from data-section
     const sectionId = button.getAttribute('data-section');
-    console.log("clicked");
     
     // Hide other sections
     document.querySelectorAll('.content-box > div').forEach((section) => {
---
+++ project.js
@@ -0,0 +1,39 @@
+export const skills = {
+  'devicon-csharp-plain': 'C#',
+  'devicon-python-plain': 'Python',
+  'devicon-sqldeveloper-plain': 'SQL',
+  'devicon-git-plain': 'Git',
+  'devicon-bash-plain': 'Bash',
+  'devicon-html5-plain': 'HTML',
+  'devicon-css3-plain': 'CSS',
+  'devicon-javascript-plain': 'Javascript',
+  'devicon-express-original': 'Express',
+  'devicon-react-original': 'React',
+  'devicon-mongodb-plain': 'MongoDB',
+  'devicon-dotnetcore-plain': 'Dotnet',
+  'devicon-kotlin-plain': 'Kotlin',
+  'devicon-linux-plain': 'Linux',
+  'devicon-javascript-plain': 'Java',
+  'devicon-docker-plain': 'Docker'
+}
+
+export const projects = [{
+  name: 'Book Reading App',
+  img: '',
+  skills: ['devicon-kotlin-plain', 'devicon-sqldeveloper-plain', 'devicon-html5-plain',
+    'devicon-git-plain'
+  ]
+},
+{
+  name: 'Olympic & AirBnb Mapping',
+  img: 'OlympicAirbnbMapping.png',
+  skills: ['devicon-react-original', 'devicon-css3-plain', 'devicon-javascript-plain', 
+    'devicon-express-original', 'devicon-mongodb-plain', 'devicon-git-plain']
+},
+{
+  name: 'Teacher/Student Scheduler',
+  img: '',
+  skills: ['devicon-python-plain', 'devicon-sqldeveloper-plain', 'devicon-html5-plain',
+    'devicon-css3-plain', 'devicon-git-plain'
+  ]
+}];
