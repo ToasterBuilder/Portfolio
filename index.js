3a9ea1be013ef954c985c332a6ff9fd8cb33d33e
Kevin Wu <wukevin1200@gmail.com>
2025-01-27 20:17:24 -0500

    Add interactivity with nav bar

--- index.js
+++ index.js
@@ -22,8 +22,25 @@ const skillsKeys = Object.keys(skills);
 const skillsSection = document.getElementsByClassName("skills-section")[0];
 
 for(let i = 0; i < skillsKeys.length; ++i){
+  // Create icons
   const icon = document.createElement('i');
   icon.className = skillsKeys[i];
   icon.textContent = skills[skillsKeys[i]]
   skillsSection.appendChild(icon);
 }
+
+document.querySelectorAll('.selector .button-start, .selector .button').forEach((button) => {
+  button.addEventListener('click', () => {
+    // Get the section to display from data-section
+    const sectionId = button.getAttribute('data-section');
+    console.log("clicked");
+    
+    // Hide other sections
+    document.querySelectorAll('.content-box > div').forEach((section) => {
+      section.style.display = 'none';
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'flex';
+  })
+});
